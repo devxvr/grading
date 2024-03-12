71f961a3fed4a758c4b455fd96cd898db0639f70
Xavier <xavier.balita007@gmail.com>
2024-03-12 16:37:34 +0800

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -4,7 +4,7 @@ class Database{
     private $host = 'localhost';
     private $username = 'root';
     private $password = '';
-    private $database = '';
+    private $database = 'gradingSystem';
     protected $connection;
 
     function connect(){

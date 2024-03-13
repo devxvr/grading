808d4cebf98ccf788552f6ee1d5335e147c82d5a
Xavier <xavier.balita007@gmail.com>
2024-03-14 04:12:20 +0800

    Update edit_component.php

--- admin/edit_component.php
+++ admin/edit_component.php
@@ -1,3 +1,4 @@
+
 <?php
 require_once './component_class.php';
 require_once './manage_component.php';
@@ -35,16 +36,44 @@ if(isset($_POST['save'])){
     }
 }
 ?>
-<div class="container-fluid">
+
+<?php
+    $title = 'Calendar';
+    require_once('../includes/head.php');
+?>
+<body>
+<?php
+    require_once('../includes/sidebar.admin.php');
+?>  
+<div class="main p-3">
+    
+    <div class="card bg-gray-500 text-dark" style="box-shadow: 0 4px 2px -2px gray;">
+        <div class="page-title mt-2 ">
+            <h2>Edit Components</h2>
+        </div>
+    </div>
+    
+    <div class="card mb-3 mt-4">
+        <div  div class="card-body">
+            
+        <div class="container-fluid">
     <form action="" id="component-form" method="post">
         <input type="hidden" name="id" value="<?php echo isset($component_id) ? $component_id : '' ?>">
-        <div class="form-group">
+        <div class="col-4 form-group">
             <label for="name" class="control-label"></label>
             <input type="text" name="name" autofocus id="name" required class="form-control form-control-sm rounded-0" value="<?php if(isset($_POST['name'])) { echo $_POST['name']; } else if (isset($component->name)){ echo $component->name; } ?>">
         </div>
         <div class="button-group">
-            <button type="submit" name="save" class="btn btn-primary mt-2 mb-3 brand-bg-color" id="addStaffButton">Save Changes</button>
+            <button type="submit" name="save" class="btn btn-primary mt-3 mb-2 brand-bg-color" id="addStaffButton">Save Changes</button>
         </div>
     </form>
 </div>
-
+        </div>
+    </div>
+</div>
+</body>
+<?php
+    
+    require_once('../includes/script.js.php');
+?>
+</html>
